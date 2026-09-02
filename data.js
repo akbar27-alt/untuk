3d6c6eba37636fa48d3cf080be83b8d60544b527
akbar27-alt <akbaragusti027@gmail.com>
2026-09-02 23:03:51 +0700

    Replace second photo URL in data.js

--- data.js
+++ data.js
@@ -35,7 +35,7 @@ const CONFIG = {
     // contoh: { src: "foto1.jpg", caption: "waktu itu..." }
     photos: [
         { src: "img/img/5b60ea73-250e-4a50-9fab-addf411ebb45.jpeg", caption: "taruh foto pertama di sini" },
-        { src: "img/foto2.jpg", caption: "taruh foto kedua di sini" },
+        { src: "img/img/04e80448-ac98-42c5-aaae-f5c6349dfb21.jpeg", caption: "taruh foto kedua di sini" },
         { src: "img/foto4.jpg", caption: "taruh foto ketiga di sini" }
     ],
 
